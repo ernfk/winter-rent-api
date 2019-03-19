9ed3b5145a328fe8d7ca99c1c81be194af9d4867
ernfk <ernest.filip.kret@gmail.com>
2019-03-19 19:26:27 +0100

    Delete: Photo and filePath from Item

--- src/main/java/com/winterrent/winterrent/entity/Item.java
+++ src/main/java/com/winterrent/winterrent/entity/Item.java
@@ -27,12 +27,6 @@ public class Item {
     @Column(name="model_no")
     private String modelNo;
 
-    @Column(name = "photo")
-    private byte[] photo;
-
-    @Transient
-    private String filePath;
-
     public Item() {
 
     }
@@ -72,22 +66,6 @@ public class Item {
 
     public void setModelNo(String modelNo) {
         this.modelNo = modelNo;
-    }
-
-    public byte[] getPhoto() {
-        return photo;
-    }
-
-    public void setPhoto(byte[] photo) {
-        this.photo = photo;
-    }
-
-    public String getFilePath() {
-        return filePath;
-    }
-
-    public void setFilePath(String filePath) {
-        this.filePath = filePath;
     }
 
     @Override

2229dfe4e28a2c24fa14547117a30a3e43312640
ernfk <ernest.filip.kret@gmail.com>
2019-03-28 07:36:00 +0100

    Refactor: Delete throwing error

--- src/main/java/com/winterrent/winterrent/service/image/ImageServiceImpl.java
+++ src/main/java/com/winterrent/winterrent/service/image/ImageServiceImpl.java
@@ -2,9 +2,6 @@ package com.winterrent.winterrent.service.image;
 
 import com.winterrent.winterrent.dao.image.ImageDAO;
 import com.winterrent.winterrent.entity.Image;
-import com.winterrent.winterrent.entity.Item;
-import com.winterrent.winterrent.service.image.exceptions.ImageNotFound;
-import com.winterrent.winterrent.service.item.exceptions.ItemNotFound;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -54,7 +51,6 @@ public class ImageServiceImpl implements ImageService {
     public Image findImage(int itemId) {
         LOGGER.info("Getting image by item id: {}", itemId);
         Optional<Image> opt = this.imageDAO.findImage(itemId);
-        opt.orElseThrow(() -> new ImageNotFound("Can not find image for item with id: " + itemId));
-        return opt.get();
+        return opt.orElse(null);
     }
 }

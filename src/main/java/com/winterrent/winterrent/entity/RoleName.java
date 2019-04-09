ce3adc2d46f60e4ed2dc9c4bfaa0249724707041
ernfk <ernest.filip.kret@gmail.com>
2019-04-09 08:13:06 +0200

    Add: Domain model for User

---
+++ src/main/java/com/winterrent/winterrent/entity/RoleName.java
@@ -0,0 +1,5 @@
+package com.winterrent.winterrent.entity;
+
+public enum RoleName {
+    ROLE_USER, ROLE_ADMIN
+}
---
+++ src/main/java/com/winterrent/winterrent/entity/User.java
@@ -0,0 +1,77 @@
+package com.winterrent.winterrent.entity;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String email;
+
+    private String password;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> role = new HashSet<>();
+
+    public User() {
+
+    }
+
+    public User(String username, String email, String password, Set<Role> role) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Role> getRole() {
+        return role;
+    }
+
+    public void setRole(Set<Role> role) {
+        this.role = role;
+    }
+}

b856c2322515ed091eb0beb65e7e63dd22df4051
ernfk <ernest.filip.kret@gmail.com>
2019-05-03 13:15:16 +0200

    Add: Initial UserDAO implementation

---
+++ src/main/java/com/winterrent/winterrent/dao/user/UserDAOHibernateImpl.java
@@ -0,0 +1,59 @@
+package com.winterrent.winterrent.dao.user;
+
+import com.winterrent.winterrent.entity.User;
+import org.hibernate.Session;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import java.util.Optional;
+
+@Repository
+public class UserDAOHibernateImpl implements UserDAO{
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public UserDAOHibernateImpl(EntityManager theEntityManager) {
+        this.entityManager = theEntityManager;
+    }
+
+    @Override
+    public Optional<User> findByUsernameOrEmail(String username, String email) {
+        Session currentSession = entityManager.unwrap(Session.class);
+        Query query = currentSession.createQuery("from User u where u.email = :email OR u.username = :username");
+        query.setParameter("email", email);
+        query.setParameter("username", username);
+        User user = (User) query.getSingleResult();
+        return Optional.ofNullable(user);
+    }
+
+    @Override
+    public Optional<User> findById(Long id) {
+        Session currentSession = entityManager.unwrap(Session.class);
+        User user = currentSession.get(User.class, id);
+        return Optional.ofNullable(user);
+    }
+
+    @Override
+    public boolean existsByUsername(String username) {
+        Session currentSession = entityManager.unwrap(Session.class);
+        User user = currentSession.get(User.class, username);
+        return user.getId() != null;
+    }
+
+    @Override
+    public boolean existsByEmail(String email) {
+        Session currentSession = entityManager.unwrap(Session.class);
+        User user = currentSession.get(User.class, email);
+        return user.getId() != null;
+    }
+
+    @Override
+    public User save(User user) {
+        Session currentSession = entityManager.unwrap(Session.class);
+        currentSession.saveOrUpdate(user);
+        return user;
+    }
+}
